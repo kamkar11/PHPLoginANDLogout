ebb3112e78beb4f7f7b8737015fac07843036a09
Kamil Machanski <kamilmachanski@gmail.com>
2019-04-03 18:41:34 +0200

    koniec pracy system logowania i wylogowywania zabezpieczony przed wstrzykiwaniem SQL

--- zaloguj.php
+++ zaloguj.php
@@ -25,25 +25,34 @@
             $login = $_POST['login'];
             $haslo = $_POST['haslo'];
 
-            $sql="SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$haslo'";
-            if($rezultat=$polaczenie->query($sql))
-            {
-            $ilu_userow=$rezultat->num_rows;
-            if($ilu_userow>0)
-            {
-              $_SESSION['zalogowany'] = true;
-              $wiersz = $rezultat->fetch_assoc();
-              $_SESSION['id'] = $wiersz['id'];
-              $_SESSION['user'] = $wiersz['user'];
-              $_SESSION['drewno'] = $wiersz['drewno'];
-              $_SESSION['kamien'] = $wiersz['kamien'];
-              $_SESSION['zboze'] = $wiersz['zboze'];
-              $_SESSION['email'] = $wiersz['email'];
-              $_SESSION['dnipremium'] = $wiersz['dnipremium'];
-              unset($_SESSION['blad']);
-              $rezultat->free_result();
-              header('Location: gra.php');
-            }
+            $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+            $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
+
+            //$sql="SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$haslo'";
+
+
+            if(($rezultat = @$polaczenie->query(
+                sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
+                mysqli_real_escape_string($polaczenie,$login),
+                mysqli_real_escape_string($polaczenie,$haslo))))){
+
+
+                $ilu_userow=$rezultat->num_rows;
+                if($ilu_userow>0)
+                {
+                  $_SESSION['zalogowany'] = true;
+                  $wiersz = $rezultat->fetch_assoc();
+                  $_SESSION['id'] = $wiersz['id'];
+                  $_SESSION['user'] = $wiersz['user'];
+                  $_SESSION['drewno'] = $wiersz['drewno'];
+                  $_SESSION['kamien'] = $wiersz['kamien'];
+                  $_SESSION['zboze'] = $wiersz['zboze'];
+                  $_SESSION['email'] = $wiersz['email'];
+                  $_SESSION['dnipremium'] = $wiersz['dnipremium'];
+                  unset($_SESSION['blad']);
+                  $rezultat->free_result();
+                  header('Location: gra.php');
+                }
             else {
               $_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
                   header('Location: index.php');
