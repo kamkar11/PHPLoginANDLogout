d278b1f5805891d29ffc2e429afb33ab6aac7e52
Kamil Machanski <kamilmachanski@gmail.com>
2019-04-03 17:20:44 +0200

    Dodanie plikow z baza danych i modyfikacja index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <title>Osadnicy - gra przeglądarkowa</title>
+  </head>
+  <body>
+    Formularz logowania do gry <strong>Osadnicy</strong><br /><br />
+    <form action="zaloguj.php" method="post">
+    Login: <br /> <input type="text" name="login" /> <br />
+    Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+    <input type="submit" value="Zaloguj się" />
+    </form>
+  </body>
+</html>
